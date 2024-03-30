c9442ad5b6dc70b156e1a576531166f33f91378e
holybaechu <holyb@holyb.xyz>
2024-03-30 19:03:13 +0900

    WIP: Add encryption support

--- src/packets/PacketEncryptionRequest.ts
+++ src/packets/PacketEncryptionRequest.ts
@@ -1,11 +1,20 @@
-import { randomBytes } from "crypto";
+import { constants, publicEncrypt, randomBytes } from "crypto";
 import { Client } from "../Client";
 import { PacketBuffer } from "../PacketBuffer";
-import yggdrasil from "yggdrasil";
+import { yggdrasil } from "@minecraft-js/yggdrasil";
+import { parseUUID } from "@minecraft-js/uuid";
 
-const server = yggdrasil.server({
-    
-})
+// https://github.com/MinecraftJS/protocol-1.8/blob/main/src/utils/parsePublicKey.ts
+// I luv u MinecraftJS <3
+function parsePublicKey(buffer: Buffer): string {
+    const arr = buffer.toString('base64').split('');
+  
+    for (let i = 0; i < arr.length; i++) if (i % 65 === 0) arr.splice(i, 0, '\n');
+  
+    return (
+      '-----BEGIN PUBLIC KEY-----' + arr.join('') + '\n-----END PUBLIC KEY-----\n'
+    );
+}
 
 export default async function(client: Client, data: PacketBuffer) {
     console.log("Enabling encryption...")
@@ -14,4 +23,21 @@ export default async function(client: Client, data: PacketBuffer) {
     const publicKey = data.readBytes();
     const verifyToken = data.readBytes();
     const sharedSecret = randomBytes(16);
+
+    console.log("Server ID:", serverId)
+    console.log("Public Key:", publicKey)
+    console.log("Verify Token:", verifyToken)
+    console.log("Shared Secret:", sharedSecret)
+
+    const key = {
+        key: parsePublicKey(publicKey),
+        padding: constants.RSA_PKCS1_PADDING
+    }
+
+    await yggdrasil.join(client.accessToken, parseUUID(client.uuid), serverId, sharedSecret, publicKey)
+
+    const packetBuffer = new PacketBuffer(Buffer.alloc(0));
+    packetBuffer.writeBytes(publicEncrypt(key, sharedSecret));
+    packetBuffer.writeBytes(publicEncrypt(key, verifyToken));
+    client.sendPacket(11, packetBuffer)
 }
--- tests/test.ts
+++ tests/test.ts
@@ -3,11 +3,15 @@ import { Client } from "../src/index";
 
 const { Authflow, Titles } = (await import('prismarine-auth')).default
 
+function addDashes(uuid: string): string {
+    return `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(16, 20)}-${uuid.slice(20)}`
+}
+
 test("test", async () => {
     const flow = new Authflow("holybaechu", "./tests/cache", { authTitle: Titles.MinecraftNintendoSwitch, deviceType: 'Nintendo', flow: 'live' })
     const response = await flow.getMinecraftJavaToken({ fetchEntitlements: true, fetchProfile: true, fetchCertificates: true })
 
-    const client = new Client(response.profile.id, response.profile.name, response.token);
+    const client = new Client(addDashes(response.profile.id), response.profile.name, response.token);
 
     client.connect();
 })
